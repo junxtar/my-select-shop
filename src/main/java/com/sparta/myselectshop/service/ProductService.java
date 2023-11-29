fce5f9d2a24afd74658171f3e96e6cad0092830a
junxtar <aa1870088@naver.com>
2023-11-29 13:06:18 +0900

    feat: 에러 메시지 관리

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -8,12 +8,15 @@ import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.ProductFolder;
 import com.sparta.myselectshop.entity.User;
 import com.sparta.myselectshop.entity.UserRoleEnum;
+import com.sparta.myselectshop.exception.ProductNotFoundException;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.FolderRepository;
 import com.sparta.myselectshop.repository.ProductFolderRepository;
 import com.sparta.myselectshop.repository.ProductRepository;
+import java.util.Locale;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -28,6 +31,7 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final FolderRepository folderRepository;
     private final ProductFolderRepository productFolderRepository;
+    private final MessageSource messageSource;
 
     public static final int MIN_MY_PRICE = 100;
 
@@ -43,11 +47,23 @@ public class ProductService {
 
         if (myprice < MIN_MY_PRICE) {
             throw new IllegalArgumentException(
-                "유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해 주세요.");
+                messageSource.getMessage(
+                    "below.min.my.price",
+                    new Integer[]{MIN_MY_PRICE},
+                    "Wrong Price",
+                    Locale.getDefault()
+                )
+            );
         }
 
         Product product = productRepository.findById(id).orElseThrow(() ->
-            new NullPointerException("해당 상품을 찾을 수 없습니다."));
+            new ProductNotFoundException(messageSource.getMessage(
+                "not.found.product",
+                null,
+                "Not Found Product",
+                Locale.getDefault()
+            ))
+        );
 
         product.update(requestDto);
 
@@ -98,7 +114,8 @@ public class ProductService {
         productFolderRepository.save(new ProductFolder(product, folder));
     }
 
-    public Page<ProductResponseDto> getProductsInFolder(Long folderId, int page, int size, String sortBy, boolean isAsc, User user) {
+    public Page<ProductResponseDto> getProductsInFolder(Long folderId, int page, int size,
+        String sortBy, boolean isAsc, User user) {
         Pageable pageable = getPageable(page, size, sortBy, isAsc);
 
         return productRepository.findAllByUserAndProductFolderList_FolderId(user, folderId,
